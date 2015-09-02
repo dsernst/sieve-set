ac8f82476da4bd8b933d88120cec68e4c1be9cb5
David Ernst <git@dsernst.com>
2015-09-01 22:13:44 -0700

    fix off-by-one error on sieve generation

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var sieveArray = require('sieve-of-eratosthenes');
 
 module.exports = function (max) {
-  return sieveArray(max).reduce(function (obj, value) {
+  return sieveArray(max + 1).reduce(function (obj, value) {
     obj[value] = true;
     return obj;
   }, {});
